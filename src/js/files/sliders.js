1c4ca4171a82b0a21ef113f1ce26e27313bf1d67
Ivan <114999150+Sychevskyii@users.noreply.github.com>
2024-01-12 22:30:59 +0200

    update slider

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -36,9 +36,9 @@ function initSliders() {
 			modules: [Navigation, Pagination],
 			observer: true,
 			observeParents: true,
-			slidesPerView: 4,
-			spaceBetween: 0,
-			//autoHeight: true,
+			slidesPerView: 'auto',
+			spaceBetween: 32,
+			autoHeight: true,
 			speed: 800,
 
 			//touchRatio: 0,
@@ -77,25 +77,25 @@ function initSliders() {
 				nextEl: '.swiper-button-next',
 			},
 			// Брейкпоінти
-			breakpoints: {
-				640: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
-				},
-				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
-				},
-				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 3,
-					spaceBetween: 0,
-				},
-			},
+			// breakpoints: {
+			// 	640: {
+			// 		slidesPerView: 1,
+			// 		spaceBetween: 0,
+			// 		autoHeight: true,
+			// 	},
+			// 	768: {
+			// 		slidesPerView: 2,
+			// 		spaceBetween: 20,
+			// 	},
+			// 	992: {
+			// 		slidesPerView: 3,
+			// 		spaceBetween: 20,
+			// 	},
+			// 	1268: {
+			// 		slidesPerView: 3,
+			// 		spaceBetween: 0,
+			// 	},
+			// },
 			// Події
 			on: {
 
